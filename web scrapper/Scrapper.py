82c5617d4e2c6f2c71099b97cf1c7bb374633af5
Sergio Moraes <sergiomoraes@users.noreply.github.com>
2019-03-02 23:32:34 -0500

    Magnet links only.
    Signed-off-by: Sergio Moraes <sergiomoraes@users.noreply.github.com>

--- web scrapper/Scrapper.py
+++ web scrapper/Scrapper.py
@@ -1,11 +1,21 @@
 import requests
+import urllib3
+import re
 from bs4 import BeautifulSoup
 
-url = 'https://horriblesubs.info/shows/mob-psycho-100-s2/'
+url = 'https://horriblesubs.info/api.php?method=getshows&type=show&showid=1206'
 response = requests.get(url, timeout=5)
+link =  []
+
 # scrapes the whole page
 content = BeautifulSoup(response.content, "html.parser")
+
 # tries to identify the specific torrent links under the hs-torrent-link class
-torrent = content.find_next_siblings(class_="dl-type hs-magnet-link")
-for links in torrent.next_element():
-    print(links)
+for torrent in content.find_all(class_="rls-link link-1080p"):
+    for link in torrent.find_all('a', attrs={'href': re.compile("^magnet:")}):
+        link.append(link.get('href'))
+        print (link)
+
+
+
+
