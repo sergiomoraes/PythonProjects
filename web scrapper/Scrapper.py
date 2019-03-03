dfb3017747f554f3449850e8eb1de14272836afe
Sergio Moraes <sergiomoraes@users.noreply.github.com>
2019-03-02 21:35:12 -0500

    More Updates. Working on specifying specific torrent links
    Signed-off-by: Sergio Moraes <sergiomoraes@users.noreply.github.com>

--- web scrapper/Scrapper.py
+++ web scrapper/Scrapper.py
@@ -1,12 +1,11 @@
-from bs4 import BeautifulSoup
 import requests
-import urllib3
+from bs4 import BeautifulSoup
 
 url = 'https://horriblesubs.info/shows/mob-psycho-100-s2/'
 response = requests.get(url, timeout=5)
 # scrapes the whole page
 content = BeautifulSoup(response.content, "html.parser")
 # tries to identify the specific torrent links under the hs-torrent-link class
-torrent = content.find(class_="dl-type hs-torrent-link")
+torrent = content.find_next_siblings(class_="dl-type hs-magnet-link")
 for links in torrent.next_element():
-    print(links['href'])
+    print(links)
