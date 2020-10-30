37098736734a6062d0ffa762e2f45fabf43e3bfd
Sergio Moraes <sergiomoraes@ieee.org>
2020-10-30 16:12:00 -0700

    n/a

--- release-notes-parser/rn-parser.py
+++
@@ -1,5 +0,0 @@
-import sharepy
-
-s = sharepy.connect("chpwa.sharepoint.com", username="sergio.moraes@chpw.org", password="s1bm2nd3P0eGr33n", auth_tld="login.microsoftonline.com")
-
-open('c:\\Users\\Sergio.Moraes\\Documents\\Github\\PythonProjects\\release-notes-parser\\feed.txt', 'wb').write(myfile)
